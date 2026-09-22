80d8a415be1ca7886938f91bc738051188779a16
Treasure Uzoma <treasureuzoma650@gmail.com>
2026-09-22 11:23:33 +0100

    refactor: remove bot-blocker middleware

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { rateLimiter } from "./middlewares/rate-limiter";
-import { blockBadUserAgents } from "./middlewares/bot-blocker";
 import { withAuth } from "./middlewares/session";
 import authRoute from "./routes/api/v1/auth";
 import type { Context } from "hono";
@@ -30,7 +29,6 @@ import { myTestWorkflow } from "./tests/workflow";
 const app = new Hono();
 
 app.use(logger());
-app.use("*", blockBadUserAgents);
 
 app.notFound((c) => {
   return c.json({ message: "Not Found" }, 404);
